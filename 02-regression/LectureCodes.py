0e9f2a9f402873325aacd446d047fa2ce29ab2c1
Irem Yucel <73019810+iremycl@users.noreply.github.com>
2022-09-13 00:10:22 -0700

    lecture notes update

--- 02-regression/LectureCodes.py
+++ 02-regression/LectureCodes.py
@@ -7,5 +7,15 @@ os.chdir("/Users/iremyucel/MLZoomCamp-1/02-regression")
 df = pd.read_csv("data.csv")
 df.head()
 
+## Data Preparation
 #Inconsistent column names
 df.columns.str.lower().str.replace(' ','_')
+
+strings = list(df.dtypes[df.dtypes == "object"].index)
+
+for col in strings:
+    df[col] = df[col].str.lower().str.replace(' ','_')
+
+df.head()
+
+## EDA
