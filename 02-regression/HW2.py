376fefdc9dee996ec27bfb7eb65764f9eaee64c1
Irem Yucel <73019810+iremycl@users.noreply.github.com>
2022-09-19 14:50:51 -0700

    HW2 Initial

---
+++ 02-regression/HW2.py
@@ -0,0 +1,139 @@
+from statistics import mean
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+os.chdir("/Users/iremyucel/MLZoomCamp-1/02-regression")
+
+df = pd.read_csv("housing.csv")
+df.columns
+base = [
+'longitude', 
+'latitude', 
+'housing_median_age', 
+'total_rooms',
+'total_bedrooms', 
+'population', 
+'households', 
+'median_income',
+'median_house_value',
+]
+
+df_base = df[base]
+df_base
+for f in base:
+    print(f, df[f].isna().sum())
+
+df_base['population'].median()
+
+n=len(df_base)
+n_val = int(len(df_base) * .2)
+n_test = int(len(df_base) * .2)
+n_train  = n - n_val - n_test
+n_val, n_test, n_train
+
+idx = np.arange(n)
+np.random.seed(42)
+np.random.shuffle(idx)
+
+df_train = df_base.iloc[idx[:n_train]]
+df_val = df_base.iloc[idx[n_train:n_train+n_val]]
+df_test = df_base.iloc[idx[n_train+n_val:]]
+
+len(df_train), len(df_val), len(df_test)
+
+df_train = df_train.reset_index(drop=True)
+df_val = df_val.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+
+X_train = df_train.values
+X_val = df_val.values
+X_test = df_test.values
+
+y_train = np.log1p(df_train['median_house_value'].values)
+y_val = np.log1p(df_val['median_house_value'].values)
+y_test = np.log1p(df_test['median_house_value'].values)
+
+del df_train['median_house_value']
+del df_val['median_house_value']
+del df_test['median_house_value']
+
+df_w0 = df_train.copy()
+df_w0['total_bedrooms'] = df_w0['total_bedrooms'].fillna(0)
+df_w0['total_bedrooms'].isna().sum()
+df_w0.mean()
+X_train_w0 =df_w0.values
+
+df_wmean = df_train.copy()
+df_wmean['total_bedrooms'] = df_wmean['total_bedrooms'].fillna(df_train['total_bedrooms'].mean())
+df_wmean['total_bedrooms'].isna().sum()
+df_wmean.mean()
+X_train_wmean =df_wmean.values
+
+def train_linear_regression(X,y):
+    #Find the w vector
+    ones = np.ones(X.shape[0])
+    X = np.column_stack([ones, X])
+    XTX =  X.T.dot(X)
+    XTX_inv = np.linalg.inv(XTX)
+    w_full = XTX_inv.dot(X.T).dot(y)
+
+    return w_full[0], w_full[1:]
+
+def rmse(y,y_pred):
+    se = (y - y_pred) ** 2
+    mse = se.mean()
+    return np.sqrt(mse)
+
+w00, w0 = train_linear_regression(X_train_w0, y_train)
+w0m, wm = train_linear_regression(X_train_wmean, y_train)
+
+y_pred0 = w00 + X_train_w0.dot(w0)
+y_predm = w0m + X_train_wmean.dot(wm)
+
+score0 = rmse(y_train, y_pred0)
+scoreMean = rmse(y_train, y_predm)
+print(score0.round(2), scoreMean.round(2))
+# 0.34131359101566755 0.3410416181032807
+
+def train_linear_regression_reg(X,y,r=0.001):
+    #Find the w vector
+    ones = np.ones(X.shape[0])
+    X = np.column_stack([ones, X])
+    
+    XTX = X.T.dot(X)
+    XTX = XTX + r * np.eye(XTX.shape[0])
+
+    XTX_inv = np.linalg.inv(XTX)
+    w_full = XTX_inv.dot(X.T).dot(y)
+
+    return w_full[0], w_full[1:]
+
+def train_linear_regression_reg(X,y,r=0.001):
+    #Find the w vector
+    ones = np.ones(X.shape[0])
+    X = np.column_stack([ones, X])
+    
+    XTX = X.T.dot(X)
+    XTX = XTX + r * np.eye(XTX.shape[0])
+
+    XTX_inv = np.linalg.inv(XTX)
+    w_full = XTX_inv.dot(X.T).dot(y)
+
+    return w_full[0], w_full[1:]
+
+X_train_w0.shape
+w
+w0
+
+rs = [0, 0.000001, 0.0001, 0.001, 0.01, 0.1, 1, 5, 10]
+r = 1
+for r in rs:
+    w_0, w = train_linear_regression_reg(X_train_w0, y_train, r=r)
+    w_0.shape, w.shape
+    y_pred = w0 + X_train_w0.dot(w)
+    score = rmse(y_val, y_pred)
+    score = round(score, 2)
+    print(r, w0, score)
