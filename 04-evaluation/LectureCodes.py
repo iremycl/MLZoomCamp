72941da48204b73d3a445272608bba32c95293cf
Irem Yucel <73019810+iremycl@users.noreply.github.com>
2022-10-21 22:42:12 -0700

    Week4

--- 04-evaluation/LectureCodes.py
+++ 04-evaluation/LectureCodes.py
@@ -10,6 +10,8 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 os.chdir("/Users/iremyucel/MLZoomCamp-1/04-evaluation")
 
+##4.1 Evaluation metrics
+
 df = pd.read_csv('../03-classification/WA_Fn-UseC_-Telco-Customer-Churn.csv')
 df.columns = df.columns.str.lower().str.replace(' ','_')
 categorical_columns = list(df.dtypes[df.dtypes=='object'].index)
@@ -73,6 +75,7 @@ y_pred = model.predict_proba(X_val)[:, 1]
 churn_decision = (y_pred >= 0.5)
 (y_val == churn_decision).mean()
 
+##4.2 Accuracy and dummy model
 scores = []
 thresholds = np.linspace(0,1,21)
 for t in thresholds:
@@ -84,6 +87,7 @@ for t in thresholds:
 
 plt.plot(thresholds, scores)
 
+##4.3 Confusion Table
 actual_positive = (y_val ==1)
 actual_negative = (y_val ==0)
 
@@ -104,3 +108,90 @@ cofusion_matrix = np.array([
 ])
 
 (cofusion_matrix/cofusion_matrix.sum()).round(2)
+
+##4.4 Precision and Recall
+# Precision: Fraction of positive examples that are correctly identified
+#Positive dediklerinin kaci gercekten pozitif?
+#Money you wasted
+# Recall: Fraction of correctly identified positive examples
+#Positive lerin kacini bulabilmissin?
+#Money you didnt spend but shd ve
+#1-Recall -> Kacirdigin pozitifler!
+r = tp / (tp+fn)
+
+##4.5 ROC Curves
+
+tpr = tp / (tp+fn)
+fpr = fp / (fp+tn)
+##FPR: Fraction of actual negatives that were incorrectly labelled as positive.
+spec = tn/ (fp+tn)
+spec
+#Specifity: tn / fp+tn : Fraction of actual negatives that were correctly labelled .
+
+#ROC Curve calculates these for all possible thresholds
+
+scores = []
+thresholds = np.linspace(0,1,101)
+
+for t in thresholds:
+    actual_positive = (y_val == 1)
+    actual_negative = (y_val == 0)
+    
+    predict_positive = (y_pred >= t)
+    predict_negative = (y_pred < t)
+    
+    tp = (predict_positive & actual_positive).sum()
+    tn = (predict_negative & actual_negative).sum()
+    
+    fp = (predict_positive & actual_negative).sum()
+    fn = (predict_negative & actual_positive).sum()
+    scores.append((t,tp,fp,fn,tn))
+
+columns = ['threshold','tp','fp','fn','tn']
+df_scores = pd.DataFrame(scores, columns= columns)
+df_scores[::10]
+df_scores['tpr'] = df_scores.tp / (df_scores.tp + df_scores.fn)
+df_scores['fpr'] = df_scores.fp / (df_scores.fp + df_scores.tn)
+ 
+plt.plot(df_scores.threshold,df_scores['tpr'], label = 'TPR' )
+plt.plot(df_scores.threshold,df_scores['fpr'], label = 'FPR' )
+plt.legend()
+
+#Compare it with a model that randomly predicts churning:
+np.random.seed(1)
+y_rand = np.random.uniform(0,1, size=len(y_val))
+((y_rand >= 0.5) == y_val).mean()
+
+def tpr_fpr_dataframe(y_val, y_pred):
+
+    scores = []
+    thresholds = np.linspace(0,1,101)
+
+    for t in thresholds:
+        actual_positive = (y_val == 1)
+        actual_negative = (y_val == 0)
+
+        predict_positive = (y_pred >= t)
+        predict_negative = (y_pred < t)
+
+        tp = (predict_positive & actual_positive).sum()
+        tn = (predict_negative & actual_negative).sum()
+
+        fp = (predict_positive & actual_negative).sum()
+        fn = (predict_negative & actual_positive).sum()
+        scores.append((t,tp,fp,fn,tn))
+       
+    columns = ['threshold','tp','fp','fn','tn']
+    df_scores = pd.DataFrame(scores, columns= columns)
+    df_scores['tpr'] = df_scores.tp / (df_scores.tp + df_scores.fn)
+    df_scores['fpr'] = df_scores.fp / (df_scores.fp + df_scores.tn)
+    
+    return df_scores
+
+df_rand = tpr_fpr_dataframe(y_val, y_rand)
+df_rand[::10]
+
+plt.plot(df_rand.threshold,df_rand['tpr'], label = 'TPR' )
+plt.plot(df_rand.threshold,df_rand['fpr'], label = 'FPR' )
+plt.legend()
+
